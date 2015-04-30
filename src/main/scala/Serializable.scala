e490aff5b14f02d8efb9906a4a93f19ff6397ad2
matt handler <matt.handler@gmail.com>
2015-04-30 16:33:10 -0400

    add typeclass for serializable types

---
+++ src/main/scala/Serializable.scala
@@ -0,0 +1,28 @@
+package com.scalypher
+
+trait Serializable[V] {
+  def toQuery(value: V): String
+
+  protected def safeWrapString(string: String): String =
+    wrapString(string.replaceAll("\"", """\\\""""))
+
+  protected def wrapString(string: String): String =
+    "\"" + string + "\""
+}
+
+object Serializable {
+  implicit object SerializableString extends Serializable[String] {
+    def toQuery(string: String): String =
+      safeWrapString(string)
+  }
+
+  implicit object SerializableInt extends Serializable[Int] {
+    def toQuery(int: Int): String =
+      int.toString
+  }
+
+  implicit object SerializableDouble extends Serializable[Double] {
+    def toQuery(double: Double): String =
+      double.toString
+  }
+}
