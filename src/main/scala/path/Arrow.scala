45cdad8e5f754965617ab4b3c1390b7662b1d6d8
matt handler <matt.handler@gmail.com>
2015-05-03 11:52:34 -0400

    simplify arrowtype definitions

--- src/main/scala/path/Arrow.scala
+++ src/main/scala/path/Arrow.scala
@@ -2,16 +2,11 @@ package com.scalypher.path
 
 import com.scalypher.types.ReferenceableMap
 
-sealed trait ArrowType {
-  def firstPart: String
-  def secondPart: String
-
+sealed case class ArrowType(firstPart: String, secondPart: String) {
   def toQuery(referenceableMap: ReferenceableMap, relationship: Option[RelationshipType] = None): String =
     firstPart + (relationship map (_.toQuery(referenceableMap)) getOrElse "") + secondPart
 }
 
-abstract class ConstantArrow(val firstPart: String, val secondPart: String)
-
-case object RightArrow extends ConstantArrow("-", "->") with ArrowType
-case object LeftArrow extends ConstantArrow("<-", "-") with ArrowType
-case object DirectionlessArrow extends ConstantArrow("-", "-") with ArrowType
+object RightArrow extends ArrowType("-", "->")
+object LeftArrow extends ArrowType("<-", "-")
+object DirectionlessArrow extends ArrowType("-", "-")
