6a4c98a17e1ffd3c4b9275e538967a05e8171e11
matt handler <matt.handler@gmail.com>
2015-05-07 14:39:32 -0400

    allow referenceable objects to be used in reference locations implicitly

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,7 +1,15 @@
 package com.originate.scalypher
 
+import where.Reference
+import where.ReferenceType
+
 package object types {
   trait Referenceable
+
+  object Referenceable {
+    implicit def toReference(referenceable: Referenceable): ReferenceType =
+      Reference(referenceable)
+  }
 
   type ReferenceableMap = Map[Referenceable, String]
 }
