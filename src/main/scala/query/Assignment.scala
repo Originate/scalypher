1fed1b5a1c19b474e71c1bea5b2b6cf9dc8516d1
matt handler <matt.handler@gmail.com>
2015-06-26 14:33:18 -0400

    create a set query and assignments

---
+++ src/main/scala/query/Assignment.scala
@@ -0,0 +1,53 @@
+package com.originate.scalypher
+
+import com.originate.scalypher.path.Node
+import com.originate.scalypher.types.Referenceable
+import com.originate.scalypher.types.ReferenceableMap
+import com.originate.scalypher.where.Reference
+import com.originate.scalypher.where.ReferenceWithProperty
+import com.originate.scalypher.where.ObjectReference
+import com.originate.scalypher.where.ValueReference
+
+sealed trait Assignment extends ToQueryWithIdentifiers {
+  def referenceables: Set[Referenceable]
+}
+
+case class RemovePropertyAssignment(reference: ReferenceWithProperty) extends Assignment {
+  def referenceables: Set[Referenceable] =
+    reference.getReferenceable.toSet
+
+  def toQuery(referenceableMap: ReferenceableMap): String =
+    s"${reference.toQuery(referenceableMap)} = NULL"
+}
+
+trait MergeableProperties { self: Referenceable =>
+  def assign(properties: Property*): MergePropertiesAssignment =
+    MergePropertiesAssignment(ObjectReference(this), properties)
+}
+
+case class MergePropertiesAssignment(
+  reference: ObjectReference,
+  properties: Seq[Property]
+) extends Assignment {
+  def referenceables: Set[Referenceable] =
+    reference.getReferenceable.toSet
+
+  def toQuery(referenceableMap: ReferenceableMap): String =
+    s"${reference.toQuery(referenceableMap)} += ${Property.toQuery(properties)}"
+}
+
+case class OverwriteAssignment(lhs: ObjectReference, rhs: ObjectReference) extends Assignment {
+  def referenceables: Set[Referenceable] =
+    Set(lhs.getReferenceable, rhs.getReferenceable).flatten
+
+  def toQuery(referenceableMap: ReferenceableMap): String =
+    s"${lhs.toQuery(referenceableMap)} = ${rhs.toQuery(referenceableMap)}"
+}
+
+case class PropertyAssignment[T](lhs: ReferenceWithProperty, rhs: ValueReference[T]) extends Assignment {
+  def referenceables: Set[Referenceable] =
+    lhs.getReferenceable.toSet
+
+  def toQuery(referenceableMap: ReferenceableMap): String =
+    s"${lhs.toQuery(referenceableMap)} = ${rhs.toQuery(referenceableMap)}"
+}
---
+++ src/main/scala/query/SetQuery.scala
@@ -0,0 +1,74 @@
+package com.originate.scalypher
+
+import com.originate.scalypher.action.ActionReference
+import com.originate.scalypher.action.ReturnAction
+import com.originate.scalypher.action.ReturnAll
+import com.originate.scalypher.action.ReturnDistinct
+import com.originate.scalypher.action.ReturnReference
+import com.originate.scalypher.path.Path
+import com.originate.scalypher.types.Referenceable
+import com.originate.scalypher.types.ReferenceableMap
+import com.originate.scalypher.where.Reference
+import com.originate.scalypher.where.Where
+
+case class SetQuery(
+  pathMatch: Path,
+  where: Option[Where],
+  assignment: Assignment,
+  rest: Seq[Assignment],
+  action: Option[ReturnAction]
+) extends Query {
+
+  private val assignments = assignment +: rest
+
+  def returns(reference: ActionReference, rest: ActionReference*): Query =
+    copy(action = Some(ReturnReference(reference, rest: _*)))
+
+  def returnDistinct(reference: ActionReference, rest: ActionReference*): Query =
+    copy(action = Some(ReturnDistinct(reference, rest: _*)))
+
+  def returnAll: Query =
+    copy(action = Some(ReturnAll))
+
+  def getReturnColumns: Set[String] =
+    action map matchActionToReturnColumns getOrElse Set.empty
+
+  def toQuery: String =
+    buildQuery(
+      Some("MATCH " + pathMatch.toQuery(referenceableMap)),
+      where map ("WHERE " + _.toQuery(referenceableMap)),
+      Some("SET " + (assignments map (_.toQuery(referenceableMap)) mkString ", ")),
+      action map (_.toQuery(referenceableMap))
+    )
+
+  protected val referenceableMap: ReferenceableMap =
+    referenceableMapWithPathWhereAndAction(
+      Seq(pathMatch),
+      where,
+      action,
+      assignments.flatMap(_.referenceables).toSet
+    )
+
+}
+
+object SetQuery {
+
+  def apply(pathMatch: Path, assignment: Assignment, rest: Assignment*): SetQuery =
+    SetQuery(pathMatch, None, assignment, rest, None)
+
+  def apply(pathMatch: Path, where: Where, assignment: Assignment, rest: Assignment*): SetQuery =
+    SetQuery(pathMatch, Some(where), assignment, rest, None)
+
+  def apply(pathMatch: Path, where: Option[Where], assignment: Assignment, rest: Assignment*): SetQuery =
+    SetQuery(pathMatch, where, assignment, rest, None)
+
+  def apply(
+    pathMatch: Path,
+    where: Where,
+    action: ReturnAction,
+    assignment: Assignment,
+    rest: Assignment*
+  ): SetQuery =
+    SetQuery(pathMatch, Some(where), assignment, rest, Some(action))
+
+}
