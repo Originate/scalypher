bef1ea89ffdad54766ea7a2e706ac459ff8940dc
matt handler <matt.handler@gmail.com>
2015-06-27 11:01:10 -0400

    adjust usage

--- src/main/scala/where/Predicate.scala
+++ src/main/scala/where/Predicate.scala
@@ -1,9 +1,20 @@
 package com.originate.scalypher.where
 
 import com.originate.scalypher.ConstantString
+import com.originate.scalypher.path.AnyNode
+import com.originate.scalypher.path.AnyRelationship
+import com.originate.scalypher.path.Path
 import com.originate.scalypher.ToQuery
+import com.originate.scalypher.types.Referenceable
 
-sealed trait Predicate extends ToQuery
+sealed trait Predicate extends ToQuery {
+  def nodes(path: Path)(f: Referenceable => Where): PredicateCondition =
+    PredicateCondition(this, Nodes(path), f)
+
+  def relationships(path: Path)(f: Referenceable => Where): PredicateCondition =
+    PredicateCondition(this, Relationships(path), f)
+}
+
 case object All extends ConstantString("ALL") with Predicate
 case object Any extends ConstantString("ANY") with Predicate
 case object NonePredicate extends ConstantString("NONE") with Predicate
