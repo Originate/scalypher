d5ab6d22a3589fb160be26e3779bf509fb93be7b
matt handler <matt.handler@gmail.com>
2015-07-01 16:04:20 -0400

    create node or relationship to subsection referenceables

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,21 +1,24 @@
 package com.originate.scalypher
 
+import com.originate.scalypher.action.ActionNodeOrRelationship
 import com.originate.scalypher.action.ActionReference
 import com.originate.scalypher.where.ObjectReference
-import com.originate.scalypher.where.Reference
+import com.originate.scalypher.where.NodeOrRelationshipReference
 
 package object types {
-  trait Referenceable {
-    def as(name: String): ActionReference =
-      ActionReference(ObjectReference(this), Some(name))
+  trait Referenceable
+
+  trait NodeOrRelationship extends Referenceable {
+    def as(name: String): ActionNodeOrRelationship =
+      ActionNodeOrRelationship(ObjectReference(this), Some(name))
   }
 
-  object Referenceable {
-    implicit def toReference(referenceable: Referenceable): Reference =
+  object NodeOrRelationship {
+    implicit def toReference(referenceable: NodeOrRelationship): NodeOrRelationshipReference =
       ObjectReference(referenceable)
 
-    implicit def toActionReference(referenceable: Referenceable): ActionReference =
-      ActionReference(referenceable)
+    implicit def toActionReference(referenceable: NodeOrRelationship): ActionNodeOrRelationship =
+      ActionNodeOrRelationship(referenceable)
   }
 
   type ReferenceableMap = Map[Referenceable, String]
