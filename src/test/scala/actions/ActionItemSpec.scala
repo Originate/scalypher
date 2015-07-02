5a8dbed15684fa565b2c3ed42bd55ee751572e04
matt handler <matt.handler@gmail.com>
2015-07-02 14:06:26 -0400

    spec action path

--- src/test/scala/actions/ActionItemSpec.scala
+++ src/test/scala/actions/ActionItemSpec.scala
@@ -1,19 +1,23 @@
 package com.originate.scalypher.test.action
 
 import com.originate.scalypher.action.ActionReference
+import com.originate.scalypher.action.ActionPath
 import com.originate.scalypher.path.AnyNode
 import com.originate.scalypher.types.IdentifiableMap
 import com.originate.scalypher.util.Exceptions.IdentifierAliasCollisionException
+import com.originate.scalypher.util.Exceptions.IdentifierDoesntExistException
 
 import org.scalatest._
 
-class ActionsSpec extends WordSpec with Matchers {
+class ActionItemSpec extends WordSpec with Matchers {
 
   val node = AnyNode()
   val nodeName = "a1"
-  val map: IdentifiableMap = Map(node -> nodeName)
+  val pathName = "a2"
+  val path = node --> AnyNode()
+  val map: IdentifiableMap = Map(node -> nodeName, path -> pathName)
 
-  "action references" must {
+  "action items" must {
 
     "add aliases inline" in {
       (node as "something").toQuery(map) shouldBe s"$nodeName AS something"
@@ -29,6 +33,28 @@ class ActionsSpec extends WordSpec with Matchers {
 
     "throw an exception when the alias collides" in {
       an [IdentifierAliasCollisionException] should be thrownBy (node as nodeName).toQuery(map)
+    }
+
+  }
+
+  "action paths" must {
+
+    val actionPath = ActionPath(path)
+
+    "add aliases inline" in {
+      (path as "path").toQuery(map) shouldBe s"$pathName AS path"
+    }
+
+    "provide 'as' as the column" in {
+      actionPath.as("path").toColumn(map) shouldBe "path"
+    }
+
+    "provide identifier as the column" in {
+      actionPath.toColumn(map) shouldBe pathName
+    }
+
+    "throw an exception if neither an 'as' or identifier are found" in {
+      an [IdentifierDoesntExistException] should be thrownBy actionPath.toColumn(Map.empty)
     }
 
   }
