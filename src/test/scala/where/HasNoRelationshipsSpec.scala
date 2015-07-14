85649b07b85f28ec9d05a3d5548a0166ab052d1a
Marcus Todd <matodd@andrew.cmu.edu>
2015-07-14 17:48:26 -0400

    Added hasNoRelationships convenience method to Node

--- src/test/scala/where/HasNoRelationshipsSpec.scala
+++ src/test/scala/where/HasNoRelationshipsSpec.scala
@@ -11,28 +11,28 @@ class HasNoRelationshipsSpec extends WordSpec with Matchers {
   "return a query to find AnyNodes with no relationships" in {
     val startNode = AnyNode()
     val identifiableMap: IdentifiableMap = Map(startNode -> "a1")
-    val condition = HasNoRelationships(startNode)
+    val condition = startNode.hasNoRelationships()
     condition.toQuery(identifiableMap) shouldBe "NOT (a1)-[]-()"
   }
 
   "return a query to find CypherNodes with no relationships" in {
     val startNode = CypherNode("name")
     val identifiableMap: IdentifiableMap = Map(startNode -> "a1")
-    val condition = HasNoRelationships(startNode)
+    val condition = startNode.hasNoRelationships()
     condition.toQuery(identifiableMap) shouldBe "NOT (a1)-[]-()"
   }
 
   "return a query to find a node with no relationships that have a certain label" in {
     val startNode = AnyNode()
     val identifiableMap: IdentifiableMap = Map(startNode -> "a1")
-    val condition = HasNoRelationships(startNode) withLabel "label"
+    val condition = startNode.hasNoRelationships() withLabel "label"
     condition.toQuery(identifiableMap) shouldBe "NOT (a1)-[:label]-()"
   }
 
   "return a query to find a node with no relationships that have multiple specified labels" in {
     val startNode = AnyNode()
     val identifiableMap: IdentifiableMap = Map(startNode -> "a1")
-    val condition = HasNoRelationships(startNode, Seq("label1", "label2", "label3"))
+    val condition = startNode.hasNoRelationships(Seq("label1", "label2", "label3"))
     condition.toQuery(identifiableMap) shouldBe "NOT (a1)-[:label1|:label2|:label3]-()"
   }
 
